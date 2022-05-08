b13f2555854df4bfdb521e780f184fce779a5a28
yutaro104625 <yutaro.s1047029@gmail.com>
2022-05-09 03:43:38 +0900

    SpringBootで作成したECサイト

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/dto/UserRepository.java
@@ -0,0 +1,16 @@
+package jp.co.internous.ecsite.model.dto;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.internous.ecsite.model.entity.User;
+
+
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+	List<User> findByUserNameAndPassword(String userName,String password);
+	
+
+}
